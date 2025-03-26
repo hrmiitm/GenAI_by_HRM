21731219c7241020730f7526725929f535e25094
hrmiitm <hrithik999999@gmail.com>
2025-03-27 02:25:44 +0530

    typedict: StaticTypeHinting but not validate

---
+++ Python/Week1/typedict.py
@@ -0,0 +1,15 @@
+# StaticTypeChecking: by mypy or Pyright
+
+# Not Validate Data, only for type hinting
+# we trust developer/llm to provide in correct format
+
+from typing import TypedDict
+
+class Movie(TypedDict):
+    name: str
+    year: int
+    casts: list[str]
+
+m1: Movie = {'name':'Kick', 'year':'32'}
+
+print(type(m1), m1)
