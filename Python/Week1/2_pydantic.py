e463c0fa07fac52329912af3239be8bdef1059e9
hrmiitm <hrithik999999@gmail.com>
2025-03-28 03:17:20 +0530

    EDA->Pandas-> Boolean Masking, Replace, Statistical

--- Python/Week1/2_pydantic.py
+++ Python/Week1/2_pydantic.py
@@ -10,7 +10,10 @@ class Student(BaseModel):
     cgpa: float = Field(gt=0, lt=10, default=5, description='A decimal value representing cgpa of a student')
 
 # new_std: Student = {'name':'Hritik', 'age':22, 'email':'a', 'cgpa':4.7} # this not enforce, don't use
-std_d = {'name':'Hritik', 'age':22, 'email':'a', 'cgpa':4.7}
+std_d = {'name':'Hritik', 'age':22, 'email':'a@e.c', 'cgpa':4.7}
 new_std = Student(**std_d)
 
 print(type(new_std), new_std)
+
+s = new_std.model_dump_json()
+s.save('./s.json')
